5c5ef023cca25b248a66f827a1a2e0bf973c7fa4
sushant12 <sushatnbajracharya@gmail.com>
2015-11-27 13:32:00 +0545

    added support for multiple password field

--- background.js
+++ background.js
@@ -1,11 +1,9 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-// alert();
-chrome.tabs.sendMessage(tab.id, {
-      command: "change_title",
-      title: "hoge"
-    },
-    function(msg) {
-      console.log("result message:", msg);
-    });
-
+  chrome.tabs.sendMessage(tab.id, {
+        command: "btn_clicked"
+      },
+      function(msg) {
+        console.log("result message:", msg);
+      }
+  );
 });
--- content.js
+++ content.js
@@ -1,8 +1,7 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  if (msg.command && (msg.command == "change_title")) {
+  if (msg.command && (msg.command == "btn_clicked")) {
     var reveal_password;
-reveal_password = document.querySelectorAll('input[type=password]');
-
-reveal_password[0].removeAttribute('type');
+    reveal_password = document.querySelectorAll('input[type=password]');
+    for(var i=0; i< reveal_password.length; i++) reveal_password[i].removeAttribute('type');
   }
 });
