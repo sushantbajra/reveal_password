e53c0b978b25a171cbd768ff8b5af3b67d20d867
sushant12 <sushatnbajracharya@gmail.com>
2015-12-02 06:39:00 +0545

    toggle password view

--- content.js
+++ content.js
@@ -1,7 +1,21 @@
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.command && (msg.command == "btn_clicked")) {
-    var reveal_password;
-    reveal_password = document.querySelectorAll('input[type=password]');
-    for(var i=0; i< reveal_password.length; i++) reveal_password[i].setAttribute('type', 'text');
+    var reveal_password,hide_password;
+
+    hide_password = document.querySelectorAll('[data-status]');
+
+    if (hide_password.length == 0){
+      reveal_password = document.querySelectorAll('input[type=password]');
+      for(var i=0; i< reveal_password.length; i++){
+        reveal_password[i].dataset.status = "revealed";
+        reveal_password[i].setAttribute('type', 'text');
+      }
+    }else{
+      for(var i=0; i< hide_password.length; i++){
+        delete hide_password[i].dataset.status;
+        hide_password[i].setAttribute('type', 'password');
+      }
+    }
+
   }
 });
